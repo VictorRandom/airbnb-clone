8865bb420105760a0b62615ec99a6627cec3a03a
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-13 12:04:56 -0300

    hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './index.css';
 import './App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-export default function Navbar(){
-    return(
-        <nav>
-            <img src="./airbnb-logo.png" alt="Airbnb-logo" className="nav--logo" />
-        </nav>
-    )
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="./airbnb-logo.png" alt="Airbnb-logo" className="nav--logo" />
+    </nav>
+  );
 }
