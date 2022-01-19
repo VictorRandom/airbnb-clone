31de24d7884addb784e12a3d2c94a77144ba6dc1
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-19 14:23:04 -0300

    passando os elementos do card por map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import './index.css';
-import './App.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
+import React from "react";
+import "./index.css";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./components/data";
 
 function App() {
+  const cardInfo = data.map((card) => {
+    // console.log(card);
+    return (
+      <Card
+        img={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-      img="./image1.png"
-      rating="5.0"
-      reviewCount="(6)"
-      title="Life lessons with Katie Zaferes"
-      price="From $136"
-      />
+      {cardInfo}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,15 @@ import React from 'react';
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={props.img} alt="card1" className="card--image" />
+            <img src={`./${props.img}`} alt="card1" className="card--image" />
             <div className="card--status">
                 <img src="./star1.png" alt="star1" className="card--star" />
                 <span>{props.rating}</span>
-                <span className="gray">{props.reviewCount} • </span>
-                <span className="gray">USA</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
             <p> {props.title}</p>
-            <p><span className="bold">{props.price}</span> / person</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
